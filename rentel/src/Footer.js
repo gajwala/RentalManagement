3e4bea432b78d9ea69889fae78bb7435f597da1b
balachandraiah gajwala <40599334+gajwala@users.noreply.github.com>
2020-09-17 07:43:34 +0530

    second commit

---
+++ rentel/src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <div className="Footer"><span className="developerClass">Developed by Balachandraiah</span></div>
+    )
+}
+export default Footer;
---
+++ rentel/src/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Welcome.scss';
+
+const Welcome = () => {
+
+    return (
+        <div className="Welcome">
+            <div className="Heading">Welcome To</div>
+            <div className="Heading">Rental Management System</div>
+            <div className="Heading-2">Please select Location</div>
+        </div>
+    )
+}
+
+export default Welcome;
